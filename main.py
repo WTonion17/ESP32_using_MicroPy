7aff1c9cab39c0aafac8f7b345ed708de3f70d0a
WTonion17 <vanthanh1752k3@gmail.com>
2024-09-14 23:58:22 +0700

    update

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from machine import Pin, ADC, I2C
-# import ssd1306
+import ssd1306
 import network
 import time
 import math
 import onewire, ds18x20
 
-# ESP32 Pin assignment 
-# i2c = I2C(0, scl=Pin(22), sda=Pin(21))
+# # ESP32 Pin assignment 
+# i2c = I2C(1, scl=Pin(22), sda=Pin(21))
 
 # oled_width = 128
 # oled_height = 64
 # oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-# oled.text('Hello, Wokwi!', 10, 10)      
+# oled.text('Hello, Wokwi!', 0, 0)      
 # oled.show()
 
 ssid = 'NHATRO BM T1S'
